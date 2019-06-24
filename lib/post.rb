88ac2ee9e8bad4f286e6825b4b903569acb84831
Tam Vu <vutam_a@hotmail.com>
2019-06-24 20:34:42 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -4,7 +4,18 @@ class Post
   @@all =[]
   def initialize(title)
     @title = title
-    @@all << @title 
+    @@all << self
   end 
   
+  def self.all
+    @@all
+  end 
+  
+  def author_name
+    if self.author
+      self.author.name 
+    else
+      nil 
+    end 
+  end
 end 
