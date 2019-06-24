982a71dfb934c2e496f76a9d4bbceb7cb8b9dc56
Tam Vu <vutam_a@hotmail.com>
2019-06-24 20:29:33 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -3,10 +3,16 @@ class Author
   
   def initialize(name)
     @name = name 
+    @posts = []
   end 
   
   def posts 
     @posts
   end 
+  
+  def add_post(post)
+    @posts << post
+    post.author = self 
+  end
   
 end 
