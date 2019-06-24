e2c4fe5f60203ffd56f29a96c45f416068f87fb7
Tam Vu <vutam_a@hotmail.com>
2019-06-24 20:36:01 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,6 @@
 class Author
   attr_accessor :name 
+  @@posts 
   
   def initialize(name)
     @name = name 
@@ -12,13 +13,19 @@ class Author
   
   def add_post(post)
     @posts << post
+    @@posts << post 
     post.author = self 
   end
   
   def add_post_by_title(title)
     post = Post.new(title)
     @posts << post 
+    @@posts << post 
     post.author = self 
+  end 
+  
+  def self.post_count
+    @@posts.size
   end 
   
   
